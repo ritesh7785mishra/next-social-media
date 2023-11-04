a44bdb46a28d2d9526f8279d2f8957db02945117
Ritesh Mishra <ritesh7785mishra@gmail.com>
2023-11-04 16:48:02 +0530

    search functionality implemented

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface Params {
 	userId: string;
@@ -81,5 +82,54 @@ export async function fetchUserPosts(userId: string) {
 		return threads;
 	} catch (error: any) {
 		throw new Error("Failed to fetch user posts:", error.message);
+	}
+}
+
+export async function fetchUsers({
+	userId,
+	searchString = "",
+	pageNumber = 1,
+	pageSize = 20,
+	sortBy = "desc",
+}: {
+	userId: string;
+	searchString?: string;
+	pageNumber?: number;
+	pageSize?: number;
+	sortBy?: SortOrder; // Correct the type definition
+}) {
+	try {
+		await connectToDB(); // Assuming connectToDB is an appropriate function to connect to the database
+
+		const skipAmount = (pageNumber - 1) * pageSize;
+
+		const regex = new RegExp(searchString, "i"); // This creates a case-insensitive regular expression from the searchString
+
+		const query: FilterQuery<typeof User> = {
+			id: { $ne: userId },
+		};
+
+		if (searchString.trim() !== "") {
+			query.$or = [
+				{ username: { $regex: regex } },
+				{ name: { $regex: regex } },
+			];
+		}
+
+		const sortOptions = { createdAt: sortBy };
+		const usersQuery = User.find(query)
+			.sort(sortOptions)
+			.skip(skipAmount)
+			.limit(pageSize);
+
+		const totalUserCount = await User.countDocuments(query); // Make sure to await this query
+
+		const users = await usersQuery.exec();
+
+		const isNext = totalUserCount > skipAmount + users.length;
+
+		return { users, isNext };
+	} catch (error: any) {
+		throw new Error(`Failed to fetch users: ${error.message}`);
 	}
 }
